4a5a8592962f7b1748d71537cf78bb3401e5ce1d
Nilton Volpato <nilton@volpa.to>
2026-09-14 14:03:41 -0700

    Embed resources on device.

--- apps/clock/build.rs
+++ apps/clock/build.rs
@@ -7,6 +7,10 @@ fn main() {
         "theme".to_string(),
         manifest_dir.join("../../crates/ui-theme/ui/theme.slint"),
     )]);
-    let config = slint_build::CompilerConfiguration::new().with_library_paths(library_paths);
+    let mut config = slint_build::CompilerConfiguration::new().with_library_paths(library_paths);
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    if target_os == "none" {
+        config = config.embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer);
+    }
     slint_build::compile_with_config("ui/clock.slint", config).unwrap();
 }
