ed176f608eb0f2f4254ed1b8ac4ac8304dd97bb6
Nilton Volpato <nilton@volpa.to>
2026-09-14 19:41:49 -0700

    Export slint theme as a library.

--- apps/clock/build.rs
+++ apps/clock/build.rs
@@ -1,13 +1,5 @@
-use std::collections::HashMap;
-use std::path::PathBuf;
-
 fn main() {
-    let manifest_dir = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
-    let library_paths = HashMap::from([(
-        "theme".to_string(),
-        manifest_dir.join("../../crates/ui-theme/ui/theme.slint"),
-    )]);
-    let mut config = slint_build::CompilerConfiguration::new().with_library_paths(library_paths);
+    let mut config = slint_build::CompilerConfiguration::new();
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
     if target_os == "none" {
         config = config.embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer);
--- crates/ui-theme/src/lib.rs
+++ crates/ui-theme/src/lib.rs
@@ -3,6 +3,7 @@
 
 #![no_std]
 
+slint::include_modules!();
 
 /// Path to the theme.slint file for consumers using `slint-build`.
 pub const THEME_SLINT_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/ui/theme.slint");
